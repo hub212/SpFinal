77795bebf39134fb7bf8155069e1ca2ad4fa2561
hub212 <shlomiza1@gmail.com>
2016-09-13 22:03:34 +0300

    Update Extraction.h

--- Extraction.h
+++ Extraction.h
@@ -13,44 +13,27 @@
 #ifndef EXTRACTION_H_
 #define EXTRACTION_H_
 
+struct sp_config_t {
+    int spNumOfImages;
+    int spNumOfFeatures;
+    int spPCADimension;
+    int spNumOfSimilarImages;
+    int spKNN;
+    int spLoggerLevel;
 
-/*
-    Saves the the extracted features into file. the file will be stored in the same directory of the image, and with the same name.
+    int assignArr[5];
 
-    @param config - configuration file
-    @param feats - the array of the extracted features
-    @param count - the length of the array 'feats'
+    bool spExtractionMode;
+    bool spMinimalGUI;
 
-    @return a negative number in case of error. 0 otherwise.
-*/
-int saveExtractedFeatures(SPConfig config ,SPPoint* feats, int count);
+    char* spImagesDirectory;
+    char* spImagesPrefix;
+    char* spImagesSuffix;
+    char* spPcaFilename;
+    char* spLoggerFilename;
 
-
-/*
-    Get image features from its corresponding '.feats' file
-
-    @param config - configuration file
-    @param feats - the index of the image we want to get the features of
-    @param pfeats - the feature array will be stored here.
-
-    @return the number of features
-*/
-int getExtractedFeatures(SPConfig config, int indexOfImage ,SPPoint** pfeats);
-
-#endif /* EXTRACTION_H_ */
-/*
- * Extraction.h
- *
- *  Created on: 31 ·‡Â‚◊ 2016
- *      Author: user
- */
-
-#include "SPConfig.h"
-#include "SPPoint.h"
-
-#ifndef EXTRACTION_H_
-#define EXTRACTION_H_
-
+    splitMethod spKDTreeSplitMethod;
+};
 
 /*
     Saves the the extracted features into file. the file will be stored in the same directory of the image, and with the same name.
